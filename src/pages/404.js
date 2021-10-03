479721ed5c7f3c4aab0bcf28921b2f1f4eb54dea
Katherine Wang <thecodinggal@gmail.com>
2021-10-03 00:47:24 -0700

    Cleaned up S3 issue, added 404 error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,29 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+import { Helmet } from "react-helmet";
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
+    <main className="text-white text-center">
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Oops! | Cosmic Monkey Club</title>
+      <meta name="description" content="404 Error"/>
+      <meta name="keywords" content="NFT, Cosmic Monkey, Cosmic Space Monkey"/>
+      <meta name="author" content="Cosmic Labs LLC" />
+    </Helmet>
+      <h1 className="text-3xl pt-8 pb-6">404 Error - Area not found</h1>
+      <Link to="/"><StaticImage
+        src="../images/spaceship-blastoff.jpeg"
+        alt="Cosmic Monkey Club"
+        placeholder="blurred"
+      /></Link>
+      <p className="py-4">
+        <Link to="/">Click here to go home</Link>.
       </p>
     </main>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
