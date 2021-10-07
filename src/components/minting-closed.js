f6a617a0a4caeaa115ea9517542c81fbda02d00c
Katherine Wang <thecodinggal@gmail.com>
2021-10-06 22:48:43 -0700

    fixing icon attempt #2

--- src/components/minting-closed.js
+++ src/components/minting-closed.js
@@ -9,7 +9,7 @@ export default function Minting(){
             <div className="bg-primary-lightblue text-black p-4 text-center mt-10 text-xl">
                 <p>Launch date to be determined.</p>
                 <p>Join the Discord to keep up to date with the latest news.</p>
-                <a href="https://discord.gg/cosmicnft" target="_blank" rel="noopener noreferrer"> <FontAwesomeIcon icon={faDiscord} className="text-5xl mt-2 text-black hover:text-white"/></a>
+                <span class="text-5xl"><a href="https://discord.gg/cosmicnft" target="_blank" rel="noopener noreferrer"> <FontAwesomeIcon icon={faDiscord} className="text-5xl mt-2 text-black hover:text-white"/></a></span>
             </div>
         </section>
     )
