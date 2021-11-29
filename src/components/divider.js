f75042b9ed78531d0330d0055dbf3ae8a852dd36
thecodinggal <thecodinggal@gmail.com>
2021-11-29 15:18:20 -0800

    Fixed dividers

--- src/components/divider.js
+++ src/components/divider.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-function Divider(){
-    return(
-    <div className="text-white py-6 border-b px-20 opacity-50"></div>
-    )
+function Divider() {
+  return (
+    <div className="m-auto max-w-full relative text-white" style={{marginTop: '80px'}}>
+      <div className="divider overflow-hidden"></div>
+    </div>
+  );
 }
 
-export default Divider
+export default Divider;
