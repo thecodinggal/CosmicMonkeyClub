a62399ed33fe1013b5d74ea174a729e45d6c6ae7
thecodinggal <thecodinggal@gmail.com>
2022-01-05 22:30:12 -0800

    Added logo to image

--- src/components/fullImageSection.js
+++ src/components/fullImageSection.js
@@ -9,11 +9,19 @@ export default function FullImageSection() {
     <main className="mx-auto">
           {/* <div className="space stars1"></div>
       <div className="space stars2 z-0"></div> */}
-      <section>
-        <div className="text-white text-4xl absolute bottom-3 w-full text-center z-50 hidden md:block">
+      <section className="relative">
+        <div className="text-white text-4xl absolute bottom-3 w-full text-center z-50 block">
         <FontAwesomeIcon icon={faAngleDoubleDown} className="transition duration-200 ease-in-out z-50 animate-slow-bounce"/>
         </div>
-        <div>
+        <div className="hidden md:block absolute bottom-0 right-0 md:z-50">
+        <StaticImage
+                  src="../images/cmc-logo1.png"
+                  alt="Cosmic Monkey Club logo"
+                  placeholder="blurred"
+                  className="w-20 md:w-72"
+                />
+        </div>
+        <div className="-mt-10">
         <Parallax y={[-15,15]}>
         <StaticImage
             src="../images/BANNER.jpeg"
